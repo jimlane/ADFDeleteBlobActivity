73426138ec8d2736a997ce628e2510c2a6133d6e
Jim Lane <jimlane@microsoft.com>
2018-06-29 09:42:09 -0400

    Add project files.

---
+++ ADFDeleteBlobActivity/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Management.DataFactories.Runtime;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ADFDelBlobActivityNS
+{
+    public class ADFDelBlobActivity : IDotNetActivity
+    {
+        /// <summary>
+        /// Execute method is the only method of IDotNetActivity interface you must implement.
+        /// In this sample, the method invokes the DeleteBlob method to perform the core logic.  
+        /// </summary>
+        public IDictionary<string, string> Execute(
+        IEnumerable<LinkedService> linkedServices,
+        IEnumerable<Dataset> datasets,
+        Activity activity,
+        IActivityLogger logger)
+        {
+
+            // declare types for input and output data stores
+            AzureStorageLinkedService inputLinkedService;
+
+            Dataset inputDataset = datasets.Single(dataset => dataset.Name == activity.Inputs.First().Name);
+
+            foreach (LinkedService ls in linkedServices)
+                logger.Write("linkedService.Name {0}", ls.Name);
+
+            // using First method instead of Single since we are using the same
+            // Azure Storage linked service for input and output.
+            inputLinkedService = linkedServices.First(
+                linkedService =>
+                linkedService.Name ==
+                inputDataset.Properties.LinkedServiceName).Properties.TypeProperties
+                as AzureStorageLinkedService;
+
+            string connectionString = inputLinkedService.ConnectionString; // To create an input storage client.
+            string folderPath = GetFolderPath(inputDataset);
+            string output = string.Empty; // for use later.
+
+            // create storage client for input. Pass the connection string.
+            CloudStorageAccount inputStorageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient inputClient = inputStorageAccount.CreateCloudBlobClient();
+
+            // initialize the continuation token before using it in the do-while loop.
+            BlobContinuationToken continuationToken = null;
+            do
+            {   // get the list of input blobs from the input storage client object.
+                BlobResultSegment blobList = inputClient.ListBlobsSegmented(folderPath,
+                                         true,
+                                         BlobListingDetails.Metadata,
+                                         null,
+                                         continuationToken,
+                                         null,
+                                         null);
+
+                // delete any blobs reference from input
+                output = DeleteBlob(blobList, logger, folderPath, ref continuationToken, "Microsoft");
+
+            } while (continuationToken != null);
+
+            // The dictionary can be used to chain custom activities together in the future.
+            // This feature is not implemented yet, so just return an empty dictionary.
+            return new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Gets the folderPath value from the input/output dataset.
+        /// </summary>
+        private static string GetFolderPath(Dataset dataArtifact)
+        {
+            if (dataArtifact == null || dataArtifact.Properties == null)
+            {
+                return null;
+            }
+
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
+            {
+                return null;
+            }
+
+            return blobDataset.FolderPath;
+        }
+
+        /// <summary>
+        /// Iterates through each blob (file) in the folder and deletes it.
+        /// </summary>
+
+        public static string DeleteBlob(BlobResultSegment Bresult, IActivityLogger logger, string folderPath, ref BlobContinuationToken token, string searchTerm)
+        {
+            string output = string.Empty;
+            logger.Write("number of blobs found: {0}", Bresult.Results.Count<IListBlobItem>());
+            foreach (IListBlobItem listBlobItem in Bresult.Results)
+            {
+                CloudBlockBlob inputBlob = listBlobItem as CloudBlockBlob;
+                if ((inputBlob != null) && (inputBlob.Name.IndexOf("$$$.$$$") == -1))
+                {
+                    inputBlob.DeleteIfExists();
+                }
+            }
+            return output;
+        }
+
+    }
+
+}
